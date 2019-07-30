0aba43269e9eaba8f6e27eda43628bf298916bdf
somayahbahkeem <iove36@hotmail.com>
2019-07-30 16:00:41 +0300

    deployment

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const expressPort = 3000
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://obscure-scrubland-48328.herokuapp.com/',
   development: `http://localhost:${expressPort}`
 }
 
